bd67678b6147d1aec8f4d0524d52e09bc9ec21d4
victorkwan <victorkwan@ucla.edu>
2015-04-22 15:20:11 -0700

    Added validations to Quest model. Note that :adventurer is not validated: we want something like `validate_existence_of` (available through plugin) but is not natively supported by rails, so no dice. 	modified:   questing_adventurer/app/models/quest.rb

--- questing_adventurer/app/models/quest.rb
+++ questing_adventurer/app/models/quest.rb
@@ -1,7 +1,12 @@
 class Quest < ActiveRecord::Base
+  validates :questgiver, :title, :price, :description,
+            :post_time, :expiration_time, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  
   belongs_to :adventurer, class_name: 'User', foreign_key: 'adventurer_id'
   belongs_to :questgiver, class_name: 'User', foreign_key: 'questgiver_id'
 
   has_many :pendings
-  has_many :pending_adventurers, through: :pendings, class_name: 'User', source: :user
+  has_many :pending_adventurers, through: :pendings, class_name: 'User',
+           source: :user
 end
