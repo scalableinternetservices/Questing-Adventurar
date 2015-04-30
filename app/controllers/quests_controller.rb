f40f46a8963ceb33bc3b4e7ad7c8776395403c6d
victorkwan <victorkwan@ucla.edu>
2015-04-29 23:37:47 -0700

    Login authentication WIP. 	modified:   app/controllers/application_controller.rb 	modified:   app/controllers/quests_controller.rb

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,5 +1,6 @@
 class QuestsController < ApplicationController
   before_action :set_quest, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /quests
   # GET /quests.json
