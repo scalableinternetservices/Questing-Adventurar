c075ca2d6b47554d08cd01de932c46831ae48a8a
jchen94 <jchen94@ucla.edu>
2015-05-14 21:36:02 -0700

    actual final commit before merge lol. 	new file:   app/assets/javascripts/activities.coffee 	new file:   app/assets/stylesheets/activities.scss 	new file:   app/controllers/activities_controller.rb 	new file:   app/helpers/activities_helper.rb 	new file:   app/views/activities/index.html.erb 	new file:   app/views/public_activity/pending/_create.html.erb 	new file:   app/views/public_activity/quest/_accept.html.erb 	new file:   app/views/public_activity/quest/_complete.html.erb 	new file:   app/views/public_activity/quest/_create.html.erb 	new file:   app/views/public_activity/quest/_pending.html.erb 	new file:   app/views/public_activity/quest/_update.html.erb 	new file:   app/views/public_activity/review/_create.html.erb 	new file:   app/views/public_activity/user/_update.html.erb 	modified:   app/views/quests/listing.html.erb 	new file:   db/migrate/20150513055009_create_activities.rb 	modified:   db/schema.rb 	new file:   test/controllers/activities_controller_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513055009) do
+ActiveRecord::Schema.define(version: 20150513150933) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -62,6 +62,9 @@ ActiveRecord::Schema.define(version: 20150513055009) do
     t.string  "gender",            limit: 255
     t.decimal "questgiver_rating",             precision: 10
     t.decimal "adventurer_rating",             precision: 10
+    t.string  "address",           limit: 255
+    t.float   "latitude",          limit: 24
+    t.float   "longitude",         limit: 24
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -70,13 +73,13 @@ ActiveRecord::Schema.define(version: 20150513055009) do
     t.integer  "questgiver_id",   limit: 4
     t.integer  "adventurer_id",   limit: 4
     t.string   "title",           limit: 255
-    t.decimal  "price",                       precision: 10
+    t.decimal  "price",           precision: 10, scale: 2
     t.string   "description",     limit: 255
     t.datetime "post_time"
     t.datetime "expiration_time"
-    t.integer  "status",          limit: 4
+    t.integer  "status",          limit: 4,                  default: 0
   end
-
+  
   add_index "quests", ["adventurer_id"], name: "fk_rails_6f9b60add0", using: :btree
   add_index "quests", ["questgiver_id"], name: "fk_rails_8d95dd76a6", using: :btree
 
