b343ed1938050bb0f0a059c5b0b084c447740f17
victorkwan <victorkwan@ucla.edu>
2015-05-15 03:41:34 -0700

    Bugfixes round infinity!

--- db/migrate/20150514185457_add_scale_to_adventurer_rating.rb
+++ db/migrate/20150514185457_add_scale_to_adventurer_rating.rb
@@ -1,5 +1,5 @@
 class AddScaleToAdventurerRating < ActiveRecord::Migration
   def change
-  	change_column :profiles, :adventurer_rating, :decimal, precision: 10, scale: 2
+  	change_column :profiles, :adventurer_rating, :decimal, precision: 10, scale: 2, default: 0
   end
 end
