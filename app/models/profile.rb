79e098d93fface56323453b33d4bb39d551568b7
victorkwan <victorkwan@ucla.edu>
2015-05-22 01:39:48 -0700

    No geocoding for Tsung.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-  after_validation :lat_changed?
+  # after_validation :lat_changed?
 
   belongs_to :user
 
